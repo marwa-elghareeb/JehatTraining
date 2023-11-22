9f807ea4dfd0a05aadc5818cb608a906affec018
marwa-elghareeb <marwa.job.mans@gmail.com>
2023-11-22 19:55:06 +0200

    update
    update

---
+++ header.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="TemplateMo">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap"
+        rel="stylesheet">
+
+    <title>الجمعية التعاونيه متعدده الاغراض   </title>
+     <!-- Favicon -->
+  <link rel="icon" type="image/x-icon" href="assets/images/favicon.png">
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendor/bootstrap/css/bootstrap.rtl.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style-ar.css">
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-finance-business.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+    <!--
+
+Finance Business TemplateMo
+
+https://templatemo.com/tm-545-finance-business
+
+-->
+</head>
+
+<body dir="rtl">
+
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+    <!-- ***** Preloader End ***** -->
+
+    <!-- Header -->
+    <div class="sub-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-xs-12">
+                    <ul class="left-info">
+                        <li><a href="#"><i class="fa fa-envelope-o"></i>alrahwa@outlook.com</a></li>
+                        <li><a href="#"><i class="fa fa-phone"></i> + 966503166657</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <ul class="right-icons">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                        <li><a href="#"><i class="fa fa-behance"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <header class="">
+        <nav class="navbar navbar-expand-lg">
+            <div class="container">
+                <a class="navbar-brand" href="index.html">
+                    <h2><img src="assets/images/logo.png"
+                        style="height: 50px; width: 160px;"></h2>
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+                    aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#top">الرئيسية
+                                <span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li class="dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true"
+                              aria-expanded="false" style="direction: rtl;">عن الجمعية </a>
+                            <div class="dropdown-menu" aria-labelledby="dropdown01">
+                              <a class="dropdown-item page-scroll" href="about.html">نبذة عن الجمعية</a>
+                              <div class="dropdown-divider"></div>
+                              <a class="dropdown-item page-scroll" href="about1.html">شهادة التسجيل</a>
+                              <div class="dropdown-divider"></div>
+                              <a class="dropdown-item page-scroll" href="about2.html">أعضاء الجمعية العمومية</a>
+                              <div class="dropdown-divider"></div>
+                              <a class="dropdown-item page-scroll" href="about3.html">مجلس الإدارة</a>
+                              <div class="dropdown-divider"></div>
+                              <a class="dropdown-item page-scroll" href="about4.html">الهيكل التنظيمي</a>
+                              <div class="dropdown-divider"></div>
+                              <a class="dropdown-item page-scroll" href="about5.html">الموظفون والموظفات</a>
+                            </div>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="Gfiles.html">ملفات الحوكمة</a>
+                          </li>
+                          <li class="dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true"
+                              aria-expanded="false" style="direction: rtl;">الخدمات الإلكترونية </a>
+                            <div class="dropdown-menu" aria-labelledby="dropdown01">
+                              <a class="dropdown-item page-scroll" href="sarvice.html">الشكاوي والإقتراحات</a>
+                              <div class="dropdown-divider"></div>
+                              <a class="dropdown-item page-scroll" href="sarvice1.html">استطلاع رضا الموظفين</a>
+                              <div class="dropdown-divider"></div>
+                              <a class="dropdown-item page-scroll" href="sarvice2.html">استطلاع رضا أعضاء مجلس الإدارة</a>
+                              <div class="dropdown-divider"></div>
+                              <a class="dropdown-item page-scroll" href="sarvice3.html">استطلاع رضا أعضاء الجمعية العمومية</a>
+                            </div>
+                          </li>
+                          <li class="dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true"
+                              aria-expanded="false" style="direction: rtl;">المركز الإعلامي </a>
+                            <div class="dropdown-menu" aria-labelledby="dropdown01">
+                              <a class="dropdown-item page-scroll" href="photo.html">مكتبة الصور</a>
+                              <div class="dropdown-divider"></div>
+                              <a class="dropdown-item page-scroll" href="video.html">مكتبة الفيديو</a>
+                              <div class="dropdown-divider"></div>
+                              <a class="dropdown-item page-scroll" href="news.html">الأخبار</a>
+                              <div class="dropdown-divider"></div>
+                              <a class="dropdown-item page-scroll" href="recruitment.html">التوظيف</a>
+                            </div>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="Connectus.html">اتصل بنا</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="rateus.html">قيمنا</a>
+                          </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
---
+++ index.php
@@ -0,0 +1,249 @@
+<?php include('header.php'); ?>
+
+ <!-- Page Content -->
+    <!-- Banner Starts Here -->
+    <div class="main-banner header-text" id="top">
+        <div class="Modern-Slider">
+            <!-- Item -->
+            <div class="item item-1">
+                <div class="img-fill">
+                    <div class="text-content">
+                        <h6>we are ready to help you</h6>
+                        <h4>Financial Analysis<br>&amp; Consulting</h4>
+                        <p>This finance HTML template is 100% free of charge provided by TemplateMo for everyone. This
+                            is a multiple-page version with different HTML pages. <a href="one-page.html">One-page
+                                version</a> is available as of 2021 February.</p>
+                        <a href="contact.html" class="filled-button">contact us</a>
+                    </div>
+                </div>
+            </div>
+            <!-- // Item -->
+            <!-- Item -->
+            <div class="item item-2">
+                <div class="img-fill">
+                    <div class="text-content">
+                        <h6>we are here to support you</h6>
+                        <h4>Accounting<br>&amp; Management</h4>
+                        <p>You are allowed to use this template for your company websites. You are NOT allowed to
+                            re-distribute this template ZIP file on any template download website. Please contact
+                            TemplateMo for more detail.</p>
+                        <a href="services.html" class="filled-button">our services</a>
+                    </div>
+                </div>
+            </div>
+            <!-- // Item -->
+            <!-- Item -->
+            <div class="item item-3">
+                <div class="img-fill">
+                    <div class="text-content">
+                        <h6>we have a solid background</h6>
+                        <h4>Market Analysis<br>&amp; Statistics</h4>
+                        <p>You can download, edit and use this layout for your business website. Phasellus lacinia ac
+                            sapien vitae dapibus. Mauris ut dapibus velit cras interdum nisl ac urna tempor mollis.</p>
+                        <a href="about.html" class="filled-button">learn more</a>
+                    </div>
+                </div>
+            </div>
+            <!-- // Item -->
+        </div>
+    </div>
+    <!-- Banner Ends Here -->
+
+    <div class="request-form">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <h4>الجمعية التعاونية متعددة الأغراض بالرهوة
+                    </h4>
+                    <span>يسعدنا استقبال طلباتكم واستفساراتكم فى اى وقت !.</span>
+                </div>
+                <div class="col-md-4">
+                    <a href="contact.html" class="border-button"> تواصل معانا</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="services">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-heading">
+                        <h2>مشاريع <em>الجمعية</em></h2>
+                        <span>برامج و مشاريع الجمعية
+                        </span>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="service-item">
+                        <img src="assets/images/111.jpg" alt="" style="height: 200px; width: 350px;">
+                        <div class="down-content">
+                            <h4>قصر رهوان للشقق الفندقية </h4>
+                            <p>قصــر رهــوان يوفــر للضيــوف الكــرام شــقق فندقيــة فاخــرة بمقاســات مختلفــة حيــث يحتــوي
+                                الفنــدق على 50 شــقة فندقيــة</p>
+                            <a href="" class="filled-button">المزيد </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="service-item">
+                        <img src="assets/images/112.jpg" alt="" style="height: 200px; width: 350px;">
+                        <div class="down-content">
+                            <h4>متاجر ومخابز ثمار</h4>
+                            <p>تقـع متاجـر ومخابـز ثمـار في نفـس المجمـع الاسـتثماري للجمعيـة التعاونيـة على امتـداد شـارع
+                                الملـك
+                                فيصـل في وسـط المدينـة</p>
+                                <a href="" class="filled-button">المزيد </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="service-item">
+                        <img src="assets/images/113.jpg" alt="" style="height: 200px; width: 350px;">
+                        <div class="down-content">
+                            <h4>المغاسل التعاونية الأوتوماتيكية</h4>
+                            <p>وحرصــا مــن الجمعيــة على تقديــم أفضل الخدمــات لقصــر رهــوان للشــقق الفندقيــة فقــد أعــدت
+                                المغاسـل التعاونيـة  </p>
+          
+                                <a href="" class="filled-button">المزيد </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="fun-facts">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="left-content">
+                        <span>من نحن</span>
+                        <h2>الجمعية التعاونية متعددة <em>الاغراض بالرهوة </em></h2>
+                        <p>تأسسـت الجمعيـة التعاونيـة متعـددة الاغـراض بالرهـوة منطقـة الباحـة بتاريـخ 22/7/1438هــ برقـم 292 بهـدف تشـجيع العمـل التعاونـي بالمنطقـة واتاحـة الفـرص لمحـدودي الدخـل مـن المسـاهمة في تنميـة المجتمـع والاسـتفادة مـن مواردهـم المحـدودة مـن خلال أنشـطة الجمعيـة وقـام علي تأسيسـها الشـيخ المهنـدس/ أحمـد علي الكلــي وعـدد مـن أبنـاء الرهـوة ووصـل عـدد مساهمــي الجمعيــة الـى 359 مساهــم بـرأس مـال قـــــدرة 700،699،8 ريـال فقـط وقـدره ثمانية مليـون وسـتمائة وتسـعه وتسـعون الفـا وسـبعمائة ريـال وقـد نجحـت في اسـتقطاب عـدد كبيـر مـن محـدودي الدخـل وعـدد مـن رجـال الأعمـال .
+                        </p>
+                        <a href="about.html" class="filled-button">للمزيد </a>
+                    </div>
+                </div>
+                <div class="col-md-6 align-self-center">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="count-area-content">
+                                <div class="count-digit">945</div>
+                                <div class="count-title">Work Hours</div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="count-area-content">
+                                <div class="count-digit">1280</div>
+                                <div class="count-title">Great Reviews</div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="count-area-content">
+                                <div class="count-digit">578</div>
+                                <div class="count-title">Projects Done</div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="count-area-content">
+                                <div class="count-digit">26</div>
+                                <div class="count-title">Awards Won</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+   
+
+    <div class="testimonials">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-heading">
+                        <h2>أخبار  <em>الجمعية </em></h2>
+                        <span>فيما يلى جميع الاخبار الخاصه بالجمعية</span>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <div class="owl-testimonials owl-carousel">
+
+                        <div class="testimonial-item">
+                            <div class="inner-content">
+                                <h4>نتائج تقييم الحوكمة 2022.م </h4>
+                                <span> 30/07/1444
+                                </span>
+                                <p>"قام ممثل فريق تقييم الحوكمة أ. ايمن سعيد الغامدي بزيارة مقر الجمعية يوم الاثنين الموافق 20/2/2023م بحضور الممثل المالي أ.محمد الفرحان والمدير التنفيذي أ. وليد بن صالح الغامدي والمحاسب أ. باسم عاطف وتم مراجعة اعمال الجمعية ومطابقتها لنظام الحوكمة من قبل ممثل الوزارة والله ولي التوفيق."</p>
+                            </div>
+                        
+                        </div>
+
+                        <div class="testimonial-item">
+                            <div class="inner-content">
+                                <h4>اجتماع الجمعية العمومية  </h4>
+                                <span>11/06/2023 </span>
+                                <p>"قام ممثل فريق تقييم الحوكمة أ. ايمن سعيد الغامدي بزيارة مقر الجمعية يوم الاثنين الموافق 20/2/2023م بحضور الممثل المالي أ.محمد الفرحان والمدير التنفيذي أ. وليد بن صالح الغامدي والمحاسب أ. باسم عاطف وتم مراجعة اعمال الجمعية ومطابقتها لنظام الحوكمة من قبل ممثل الوزارة والله ولي التوفيق."</p>
+
+                            </div>
+                         
+                        </div>
+
+                        <div class="testimonial-item">
+                            <div class="inner-content">
+                                <h4>دعوة </h4>
+                                <span>11/06/2023 </span>
+                                <p>"قام ممثل فريق تقييم الحوكمة أ. ايمن سعيد الغامدي بزيارة مقر الجمعية يوم الاثنين الموافق 20/2/2023م بحضور الممثل المالي أ.محمد الفرحان والمدير التنفيذي أ. وليد بن صالح الغامدي والمحاسب أ. باسم عاطف وتم مراجعة اعمال الجمعية ومطابقتها لنظام الحوكمة من قبل ممثل الوزارة والله ولي التوفيق."</p>
+
+                            </div>
+                          
+                        </div>
+
+                       
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="partners">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="owl-partners owl-carousel">
+
+                        <div class="partner-item">
+                            <img src="assets/images/client-01.png" title="1" alt="1">
+                        </div>
+
+                        <div class="partner-item">
+                            <img src="assets/images/client-01.png" title="2" alt="2">
+                        </div>
+
+                        <div class="partner-item">
+                            <img src="assets/images/client-01.png" title="3" alt="3">
+                        </div>
+
+                        <div class="partner-item">
+                            <img src="assets/images/client-01.png" title="4" alt="4">
+                        </div>
+
+                        <div class="partner-item">
+                            <img src="assets/images/client-01.png" title="5" alt="5">
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+<?php include('footer.php'); ?>
